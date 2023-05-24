cf17fdd0346549c6bea3ee7a1c08219d0ac5fdd1
Muhammad Haroon <m_haroon96@hotmail.com>
2023-05-23 17:14:30 -0700

    fixed description grab issue

--- reels/example.py
+++ reels/example.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 driver = ReelsDriver(browser='chrome', verbose=True, profile_dir='profiles/test')
 
 
-driver.login('christopher_wright_743@youtubeaudit.com', 'Yuio1234!')
+# driver.login('christopher_wright_743@youtubeaudit.com', 'Yuio1234!')
 driver.goto_shorts()
 
 
--- reels/helpers.py
+++ reels/helpers.py
@@ -11,7 +11,7 @@ class Short:
         self.url = url
         self.shortId = re.search(r'reels/(.*)?/$', url).group(1)
         self.metadata = dict(
-            description=extract_text(elem, 'div', css_classes['description']),
+            description=extract_description(elem),
             likes=extract_text(elem, 'div', css_classes['likes+comments'], 0),
             comments=extract_text(elem, 'div', css_classes['likes+comments'], 1),
             author=extract_text(elem, 'span', css_classes['author']),
@@ -31,3 +31,15 @@ class ShortUnavailableException(Exception):
 def extract_text(elem, elem_type, css, index=0):
     try: return elem.find_elements(By.XPATH, ".//%s[contains(@class, '%s')]" % (elem_type, css))[index].text
     except: return ''
+
+def extract_description(elem):
+    # try to click on "more"
+    try: elem.find_element(By.XPATH, ".//span[contains(@class, 'x1rg5ohu xsgj6o6 x1c4vz4f x2lah0s xdl72j9 xlej3yl')]").click()
+    except: pass
+    # extract expanded description
+    try: return elem.find_element(By.XPATH, ".//div[contains(@class, 'x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh xw7yly9 x1uhb9sk xw2csxc x1odjw0f xs83m0k x1c4vz4f xdt5ytf xqjyukv x1qjc9v5 x1oa3qoh x1nhvcw1')]").text
+    except: pass
+    # extract non-expanded description
+    try: return elem.find_element(By.XPATH, ".//div[contains(@class, 'x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh xw7yly9 x1n2onr6 x1plvlek xryxfnj x1c4vz4f x2lah0s xdt5ytf xqjyukv x1qjc9v5 x1oa3qoh x1nhvcw1')]").text
+    except: pass
+    return ''
