0f409224a3ba3361c2f4de6f164c573df1ab715b
Muhammad Haroon <m_haroon96@hotmail.com>
2023-05-23 18:36:11 -0700

    optimized

--- reels/helpers.py
+++ reels/helpers.py
@@ -35,12 +35,12 @@ def extract_text(elem, elem_type, css, index=0):
 def extract_description(elem):
     # extract expanded description
     try: 
-        elem.find_element(By.XPATH, ".//span[contains(@class, 'x1rg5ohu xsgj6o6 x1c4vz4f x2lah0s xdl72j9 xlej3yl')]").click()
-        return elem.find_element(By.XPATH, ".//div[contains(@class, 'x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh xw7yly9 x1uhb9sk xw2csxc x1odjw0f xs83m0k x1c4vz4f xdt5ytf xqjyukv x1qjc9v5 x1oa3qoh x1nhvcw1')]").text
+        elem.find_element(By.XPATH, ".//span[@class='x1rg5ohu xsgj6o6 x1c4vz4f x2lah0s xdl72j9 xlej3yl']").click()
+        return elem.find_element(By.XPATH, ".//div[@class='x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh xw7yly9 x1uhb9sk xw2csxc x1odjw0f xs83m0k x1c4vz4f xdt5ytf xqjyukv x1qjc9v5 x1oa3qoh x1nhvcw1']").text
     except: pass
 
     # extract non-expanded description
-    try: return elem.find_element(By.XPATH, ".//div[contains(@class, 'x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh xw7yly9 x1n2onr6 x1plvlek xryxfnj x1c4vz4f x2lah0s xdt5ytf xqjyukv x1qjc9v5 x1oa3qoh x1nhvcw1')]").text
+    try: return elem.find_element(By.XPATH, ".//div[@class='x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh xw7yly9 x1n2onr6 x1plvlek xryxfnj x1c4vz4f x2lah0s xdt5ytf xqjyukv x1qjc9v5 x1oa3qoh x1nhvcw1']").text
     except: pass
     
     return ''
